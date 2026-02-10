dc182beb215f2b65dff6c4d1eb3ebdfcd9660c4e
Rosie Pope <rosiempope@hotmail.com>
2026-02-10 17:37:25 +0000

    setting up the structure for sensor simulation

---
+++ backend/app/models.py
@@ -0,0 +1,5 @@
+##### This is where every simulated reading will eventually go.
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from database import Base
+from datetime import datetime
+
---
+++ backend/app/services/simulate.py
@@ -0,0 +1,3 @@
+#this is making fake data for testing purposes, it is not used in production. 
+#It is trying to make temperature, pressure & flow data for a given number of hours, with a given frequency.
+
